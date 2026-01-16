f4d11ae8181095217cc2f84eb2c97c7fbc759146
andre souza <andssouzza@gmail.com>
2026-01-16 19:52:53 -0400

    refactor(customer): updated name in class repository for customer

--- src/application/usecases/create-customer.usecase.spec.ts
+++ src/application/usecases/create-customer.usecase.spec.ts
@@ -1,12 +1,11 @@
 import { describe, expect, it } from '@jest/globals'
 import { Customer } from '../../domain/entities/customer.entity'
-import { CustomerRepositoryImpl } from '../../infrastructure/repositories/customer.impl.repository'
+import { InMemoryCustomerRepository } from '../../infrastructure/repositories/in-memory-customer.repository'
 import { CreateCustomerUseCase } from './create-customer.usecase'
 
 describe('CreateCustomerUseCase', () => {
   it('should create a customer successfully', async () => {
-    // TODO: implement customer creation logic
-    const customerRepository = new CustomerRepositoryImpl()
+    const customerRepository = new InMemoryCustomerRepository()
     const useCase = new CreateCustomerUseCase(customerRepository)
 
     const newCustomerData = new Customer({
--- src/application/usecases/create-customer.usecase.ts
+++ src/application/usecases/create-customer.usecase.ts
@@ -5,9 +5,6 @@ export class CreateCustomerUseCase {
   constructor(private readonly customerRepository: CustomerRepository) {}
 
   async execute(input: Customer): Promise<void> {
-    // TODO: implement customer creation logic
-
     await this.customerRepository.create(input)
-    console.log('Creating customer with input', input)
   }
 }
