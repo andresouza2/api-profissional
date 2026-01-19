50c3d404a5278327c835e01a0dfec41330c2a91d
andre souza <andssouzza@gmail.com>
2026-01-19 00:03:44 -0400

    feat(product): add product entity
    adicionando product entity

---
+++ src/domain/entities/product/product.entity.ts
@@ -0,0 +1,62 @@
+import { AggregateRoot } from '../../../core/AggregateRoot'
+import { UniqueEntityId } from '../../../core/UniqueEntityId'
+
+interface ProductProps {
+  name: string
+  description: string
+  sku: string
+  price: number
+  stock: number
+  currency: string
+  isActive?: boolean
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class Product extends AggregateRoot<ProductProps> {
+  constructor(props: ProductProps, id?: UniqueEntityId) {
+    super(
+      {
+        ...props,
+        isActive: props.isActive ?? true,
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: props.updatedAt ?? new Date(),
+      },
+      id,
+    )
+    this.validate()
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+  get description(): string {
+    return this.props.description
+  }
+  get sku(): string {
+    return this.props.sku
+  }
+  get price(): number {
+    return this.props.price
+  }
+  get stock(): number {
+    return this.props.stock
+  }
+  get currency(): string {
+    return this.props.currency
+  }
+  get isActive(): boolean {
+    return this.props.isActive!
+  }
+  get createdAt(): Date {
+    return this.props.createdAt!
+  }
+
+  private touch(): void {
+    this.props.updatedAt = new Date()
+  }
+
+  private validate(): void {
+    // Add validation logic here if needed
+  }
+}
