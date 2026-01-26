dd2405d6f7c24bc1a10e2b0d5f4f21fb1363f3b5
andre souza <andssouzza@gmail.com>
2026-01-26 10:21:51 -0400

    feat(product): add repository

---
+++ src/application/repositories/product/product.repository.ts
@@ -0,0 +1,6 @@
+import { Product } from '../../../domain/entities/product/product.entity'
+
+export abstract class ProductRepository {
+  abstract create(product: Product): Promise<void>
+  abstract findById(id: string): Promise<Product | null>
+}
