ee55e3656ecfc6ec439efdb220304db279df6664
andre souza <andssouzza@gmail.com>
2026-02-03 21:10:36 -0400

    fix: corrigindo configuração de teste

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,16 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'json'],
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@generated/(.*)$': '<rootDir>/src/generated/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    '^@shareds/(.*)$': '<rootDir>/src/shareds/$1',
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
@@ -13,6 +23,8 @@ export default {
       'ts-jest',
       {
         useESM: true,
+        tsconfig: 'tsconfig.spec.json',
+        isolatedModules: true,
       },
     ],
   },
